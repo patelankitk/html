f535d3785d140f6c94251a6daa9e442bd2a898fc
patelankitk <patelankitk@yahoo.com>
2016-04-18 15:24:46 +0100

    multiple changes

---
+++ hw1.js
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+
+<body>
+    
+<script>
+    
+    // creating a variable event1 which stores the input on     
+    var event1 = document.getElementById("button1");
+    
+     // Event listener - Add an event listener which invokes the myfirst function when the webpage first load - the window object has a method called addeventlistener which accepts 3 arguments.
+    event1.addEventListener('click',myfirst,false);
+    
+    function myfirst(){
+        
+        //storing the users inout into local variable called userinput1
+        var userinput1 =document.getElementById("input1").value;
+        
+        //writing the local variable back to html DOM where element has an id of demo 
+        document.getElementById("demo").innerHTML =userinput1;
+    }
+   
+    </script>
+  
+ 
+</body>
+
+
+
+</html>
